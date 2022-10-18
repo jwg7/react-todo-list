2442bb7005b6c548bbbaca610ea97d1233438895
John G <johngouldman@pm.me>
2022-10-18 17:51:15 -0500

    part 1

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ const ToDoItem = (props) => {
       <h2>{props.toDo.title}</h2>
       <p>Priority: {props.toDo.priority}</p>
       <p>Creation Date: {props.toDo.creationDate}</p>
-      <p>Completed Date: {props.toDo.completedDate}</p>
+      {props.toDo.completedDate && (
+        <p>Completed Date: {props.toDo.completedDate}</p>
+      )}
       <p>Description: {props.toDo.description}</p>
     </div>
   );
@@ -34,32 +36,46 @@ const ToDoItem = (props) => {
 /////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
 
-const ToDoForm = () => {
-  const [title, setTitle] = useState("")
-  const [priority, setPriority] = useState("")
-  const [description, setDescription] = useState("")
+const ToDoForm = (props) => {
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState("");
+  const [description, setDescription] = useState("");
   return (
     <div>
-<label>Title: </label>
-<input type="text" onChange={(e)=>{
-setTitle(e.target.value)
-}}></input>
-<br/>
-<label>Priority: </label>
-<select onChange={(e)=>{
-  setPriority(e.target.value)
-}}>
-<option>Select One</option>
-<option value='Low'>Low</option>
-<option value='Medium'>Medium</option>
-<option value='High'>High</option>
-</select>
-
-<br/>
+      <label>Title: </label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      ></input>
+      <br />
+      <label>Priority: </label>
+      <select
+        onChange={(e) => {
+          setPriority(e.target.value);
+        }}
+      >
+        <option>Select One</option>
+        <option value="Low">Low</option>
+        <option value="Medium">Medium</option>
+        <option value="High">High</option>
+      </select>
+      <br />
+      <label>Description: </label>
+      <textarea
+        onChange={(event) => {
+          setDescription(event.target.value);
+        }}
+      />
+      <br />
+      <button onClick={()=>{
+          props.handleAddToDo(title, priority, description)
+        }}>Add ToDo
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 ///////////////////////////////////////////////////////
 //////////////////////////////////////////////////////
@@ -75,6 +91,20 @@ const App = () => {
       completedDate: null,
     },
   ]);
+
+  const handleAddToDo = (title, priority, description) => {
+    const newToDo = {
+      title: title,
+      priority: priority,
+      isComplete: false,
+      description: description,
+      creationDate: new Date().toString(),
+      completedDate: null,
+    };
+    const toDoListCopy = [...toDoList, newToDo];
+   setToDoList(toDoListCopy);
+  };
+
   return (
     <div className="App-header">
       <ToDoForm />
